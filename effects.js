87ee7e2baf4927f2ec8d28642cf4b0c2cb01bb73
lad475 <lad475@nyu.edu>
2018-02-21 16:24:17 -0500

    Added hovering

--- effects.js
+++ effects.js
@@ -6,7 +6,17 @@ function main(){
 function divListener(){
 	//Write listeners for when
 	//the user hovers over a div, to flip it around
-	
+	let divs = document.querySelectorAll(".closed");
+	for(let i = 0; i < divs.length; i++){
+		divs[i].addEventListener("mouseover", function(evt){
+			evt.preventDefault();
+			divs[i].style['background-color'] = "#6900CC";
+		});
+		divs[i].addEventListener('mouseout', function(evt){
+			evt.preventDefault();
+			divs[i].style['background-color'] = "#350066";
+		});
+	}
 }
 
 document.addEventListener("DOMContentLoaded", main);
