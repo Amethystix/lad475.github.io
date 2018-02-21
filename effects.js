4186c175bbaa31893f8f7a255c704c1cc8379a5d
lad475 <lad475@nyu.edu>
2018-02-21 13:55:05 -0500

    Added some fonts, testing them out

--- effects.js
+++ effects.js
@@ -1,7 +1,12 @@
+
+
 function main(){
-	
+	divListener();
 }
 function divListener(){
 	//Write listeners for when
 	//the user hovers over a div, to flip it around
+	
 }
+
+document.addEventListener("DOMContentLoaded", main);
